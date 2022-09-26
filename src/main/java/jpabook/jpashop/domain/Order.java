1f7fb04779d41f8432a4d7eb1c561ad2844f88de
noname_dev <noname_dev@naver.com>
2022-09-26 20:28:03 +0900

    엔티티 클래스 개발2

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -25,7 +25,8 @@ public class Order {
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
     private LocalDateTime orderDate; //주문 시간

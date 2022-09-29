cac86b6cc520fe2fcc11e5927e258c412031f37a
noname_dev <noname_dev@naver.com>
2022-09-29 16:30:25 +0900

    주문 검색 기능 개발 //JPA Criteria를 사용했는데 실무에서는 거의 사용하지 않아 추후에 queryDSL로 바꿀 예정

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+//protected 생성자를 넣은 이유는 생성 메서드만 이용하게 하려고 막아둔 것이다.
+//@NoArgsConstructor(access = AccessLevel.PROTECTED) protected 생성자를 만들어 주는 롬복
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
